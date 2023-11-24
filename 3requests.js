087643266e90a82d5cf195b749db721e1f283538
Madhur Gaur <madhurgaur9140@gmail.com>
2023-11-24 22:26:43 +0530

    YOoo

--- 3requests.js
+++ 3requests.js
@@ -24,7 +24,8 @@ async function main() {
 server.use(cors())
 server.use(express.json()); //builtin middleware, body parser
 server.use(express.static(process.env.PUBLIC_DIR));
-app.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/public'));
+console.log(__dirname)
 server.use('/', async (req,res)=> {
   res.json("Yoooo!")
 })
