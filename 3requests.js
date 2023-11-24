e7dec51623c9c42345042176ee9ce7e8512ac80e
Madhur Gaur <madhurgaur9140@gmail.com>
2023-11-24 22:04:49 +0530

    Yo

--- 3requests.js
+++ 3requests.js
@@ -24,7 +24,10 @@ async function main() {
 server.use(cors())
 server.use(express.json()); //builtin middleware, body parser
 server.use(express.static(process.env.PUBLIC_DIR));
-app.use("/node_modules", express.static('node_modules'));
+server.use("/node_modules", express.static('node_modules'));
+server.use('/', async (req,res)=> {
+  res.json("Yoooo!")
+})
 server.use('/api/quotes',quoteRouter.routes);
 server.use('/api/users',userRouter.routes);
 server.use('*',(req,res)=>{
